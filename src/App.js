e38c893b3f3fe97412fa7520b0b45ad4a740af02
udaykumarj21 <109064277+udaykumarj21@users.noreply.github.com>
2022-07-18 19:42:52 +0530

    States React State useState

--- src/App.js
+++ src/App.js
@@ -1,65 +1,110 @@
+import { isArguments } from 'lodash';
 import * as React from 'react';
+//Made this block scoped in App component
+// const list = [
+//   {
+//     title: 'Book1',
+//     id: 1,
+//     pages: 250,
+//     cost: 800.0,
+//     site: 'mikybooks.com'
+//   },
+//   {
+//     title: 'Book2',
+//     id: 2,
+//     pages: 200,
+//     cost: 800.0,
+//     site: 'mikybooks.com'
+//   }
+// ];
 
-const list = [
-  {
-    title: 'Book1',
-    id: 1,
-    pages: 250,
-    cost: 800.0,
-    site: 'mikybooks.com'
-  },
-  {
-    title: 'Book2',
-    id: 2,
-    pages: 200,
-    cost: 800.0,
-    site: 'mikybooks.com'
-  }
-];
+function ResComps() {
+  const stories = [
+    {
+      title: 'Book1',
+      id: 1,
+      pages: 250,
+      cost: 800.0,
+      site: 'mikybooks.com'
+    },
+    {
+      title: 'Book2',
+      id: 2,
+      pages: 200,
+      cost: 800.0,
+      site: 'mikybooks.com'
+    }
+  ];
+  return <List myList={stories} />;
+}
 
-function App() {
+function List(props) {
   return (
     <div>
       <ul>
-        {list.map(function (i) {
-          return (
-            <li id={i.id}>
-              <span>{i.title}</span>
-              <br />
-              <span>{i.pages}</span>
-              <br />
-              <span>
-                <a href={i.site}>{i.site}</a>
-              </span>
-            </li>
-          );
+        {props.myList.map(function (i) {
+          return <ItemSorter key={i.key} item={i} />;
         })}
       </ul>
     </div>
   );
 }
 
-function Search() {
+const ItemSorter = (props) => {
+  const item = props.item;
   return (
-    <span>
-      <Title />
-      <label htmlFor="search">search</label>
-      <input id="search" type="text" />
-    </span>
+    <ul>
+      <li key={props.key}>
+        {item.title} <br />
+        {item.pages}
+        <br />
+        <a href={item.site}>{item.site}</a>
+      </li>
+    </ul>
   );
-}
+};
+
+// function Search() {
+//   return (
+//     <span>
+//       <Title />
+//       <label htmlFor="search">search</label>
+//       <input id="search" type="text" />
+//     </span>
+//   );
+// }
 
 function Title() {
   return <h1>Welcome To Micky Books</h1>;
 }
 
-function Main() {
+function App() {
   return (
     <div>
       <Search />
-      <App />
+      <ResComps />
     </div>
   );
 }
 
-export default Main;
+//Handler Function in JSX
+
+function Search() {
+  let temp = '';
+  const [str, setUpdatedStr] = React.useState(temp);
+
+  const hadlerConstant = (event) => {
+    setUpdatedStr(event.target.value);
+  };
+  console.log(str);
+  return (
+    <span>
+      <Title />
+      <label htmlFor="search">search</label>
+      <input id="search" type="text" onChange={hadlerConstant} />
+      <p>Searching for: {str}</p>
+    </span>
+  );
+}
+
+export default App;
